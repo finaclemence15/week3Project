cba02fa13fe8e184d94dcad0bbafafe881ee692b
finaclemence15 <finaclemence15@gmail.com>
2019-07-26 16:03:00 +0200

    hide a paragraph

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $("img#icon1").click(function() {
+      $(".p1").toggle();
+    });
+    $("img#icon2").click(function() {
+        $(".p2").toggle();
+      });
+      $("img#icon3").click(function() {
+        $(".p3").toggle();
+      });
+  });
+  
