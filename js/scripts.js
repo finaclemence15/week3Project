fc74b508b07e0dc6cbdcab9e3f60085f4b941238
finaclemence15 <finaclemence15@gmail.com>
2019-07-28 01:08:51 +0200

    adding hover to images

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+    // Toggle function
+
 $(document).ready(function() {
     $("img#icon1").click(function() {
       $(".p1").toggle();
@@ -8,5 +10,86 @@ $(document).ready(function() {
       $("img#icon3").click(function() {
         $(".p3").toggle();
       });
-  });
-  
+
+    // Image Hover
+    
+    $(".project1").hide();
+    $("#work4").css({opacity:2});
+    $("#img4").hover(function(){
+    $(this).css({opacity:0.1});
+    $(".project1").fadeIn();
+    }, function(){
+    $(this).stop().css({opacity:2})
+    $(".project1").fadeOut();
+    });
+
+    $(".project2").hide();
+    $("#work3").css({opacity:2});
+    $("#img3").hover(function(){
+    $(this).css({opacity:0.1});
+    $(".project2").fadeIn();
+    }, function(){
+    $(this).stop().css({opacity:2})
+    $(".project2").fadeOut();
+    });
+
+    $(".project3").hide();
+    $("#work2").css({opacity:2});
+    $("#img2").hover(function(){
+    $(this).css({opacity:0.1});
+    $(".project3").fadeIn();
+    }, function(){
+    $(this).stop().css({opacity:2})
+    $(".project3").fadeOut();
+    });
+
+    $(".project4").hide();
+    $("#work1").css({opacity:2});
+    $("#img1").hover(function(){
+    $(this).css({opacity:0.1});
+    $(".project4").fadeIn();
+    }, function(){
+    $(this).stop().css({opacity:2})
+    $(".project4").fadeOut();
+    });
+
+    $(".project5").hide();
+    $("#work5").css({opacity:2});
+    $("#img5").hover(function(){
+    $(this).css({opacity:0.1});
+    $(".project5").fadeIn();
+    }, function(){
+    $(this).stop().css({opacity:2})
+    $(".project5").fadeOut();
+    });
+
+    $(".project6").hide();
+    $("#work6").css({opacity:2});
+    $("#img6").hover(function(){
+    $(this).css({opacity:0.1});
+    $(".project6").fadeIn();
+    }, function(){
+    $(this).stop().css({opacity:2})
+    $(".project6").fadeOut();
+    });
+
+    $(".project7").hide();
+    $("#work7").css({opacity:2});
+    $("#img7").hover(function(){
+    $(this).css({opacity:0.1});
+    $(".project7").fadeIn();
+    }, function(){
+    $(this).stop().css({opacity:2})
+    $(".project7").fadeOut();
+    });
+
+    $(".project8").hide();
+    $("#work8").css({opacity:2});
+    $("#img8").hover(function(){
+    $(this).css({opacity:0.1});
+    $(".project8").fadeIn();
+    }, function(){
+    $(this).stop().css({opacity:2})
+    $(".project8").fadeOut();
+    });
+});
